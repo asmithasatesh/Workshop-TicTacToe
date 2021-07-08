39faf77f469268bbdc971e9bdfc99a7834a1a07f
asmithasatesh <asmithasatesh@gmail.com>
2021-07-08 12:38:55 +0530

    [Asmitha]Updated UC2 Computing input for Computer(X/O)

--- TicTacToe/TicTacToeGame.cs
+++ TicTacToe/TicTacToeGame.cs
@@ -6,26 +6,39 @@ namespace TicTacToe
     {
         public static char[] BoardInitialise()
         {
-            char[] board = { '0', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1' };
-            foreach (char i in board)
+            char[] board = new char[10];
+            for(int i=0;i<board.Length;i++)
             {
+                board[i] = ' ';
                 Console.WriteLine("Element is: {0}\t",i);
             }
             return board;
         }
+
         public static char GetPlayerInput()
         {
             Console.WriteLine("\nChoose X or O");
             char player =Convert.ToChar(Console.ReadLine());
             return player;
         }
+        public static char CalculateComputerInput(char player)
+        {
+            if (player == 'X' || player == 'x')
+                return (Convert.ToChar(Convert.ToInt32(player) - 9));
+            else
+            {
+                return (Convert.ToChar(Convert.ToInt32(player) + 9));
+            }
+        }
+
         static void Main(string[] args)
         {
-    
+            
             Console.WriteLine("welcome to TicTacToe!");
             char[] board= BoardInitialise();
-            char player1 = GetPlayerInput();
-            Console.WriteLine("Player has chosen {0}", player1);
+            char player = GetPlayerInput();
+            char computer = CalculateComputerInput(player);
+            Console.WriteLine("Player has chosen {0} and computer has chosen {1}", player,computer);
 
         }
     }
